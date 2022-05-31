f3f6f8e0903d7db79de34c28bc90a60050f11d16
rohraut <rohanraut372@gmail.com>
2022-05-31 20:03:29 +0530

    31 may

--- src/main/java/com/ijmeet/pages/HomePageTests.java
+++ src/main/java/com/ijmeet/pages/HomePageTests.java
@@ -2,9 +2,8 @@ package com.ijmeet.pages;
 
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
-import org.openqa.selenium.support.PageFactory;
 
-import com.one.UIKeywords;
+
 
 public class HomePageTests {
 
